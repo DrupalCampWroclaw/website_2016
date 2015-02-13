4ab2077d66278036104cf97c768969ed7cb177ea
grzegorzbartman <grzegorz.bartman@openbit.pl>
2015-02-13 12:07:48 +0100

    Issue #0: Sessions block - links.

--- app/sites/all/modules/custom/layout/sessions.tpl.php
+++ app/sites/all/modules/custom/layout/sessions.tpl.php
@@ -1,7 +1,8 @@
 <div id="sessions-links">
   <ul>
-    <li><?php print l(t('Videos'), 'videos'); ?></li>
-    <li><?php print l(t('Accepted Sessions'), 'sessions/accepted'); ?></li>
+    <?php /*<li><?php print l(t('Videos'), 'videos'); ?></li>*/ ?>
+    <li><?php print l(t('Submit a Session'), 'node/add/session'); ?></li>
     <li><?php print l(t('Proposed Sessions'), 'sessions/proposed'); ?></li>
+<?php /*<li><?php print l(t('Accepted Sessions'), 'sessions/accepted'); ?></li>*/ ?>
   </ul>
 </div>
