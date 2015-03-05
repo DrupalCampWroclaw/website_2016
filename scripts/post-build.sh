841c63c7b7352460971ef87534f56cfbb1f0fe92
grzegorzbartman <grzegorz.bartman@openbit.pl>
2015-03-05 12:02:26 +0100

    Issue #0: Stage proxy module. Enable stage proxy in post build.

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -4,17 +4,11 @@ echo "Post-build script start"
 # Add custom code here
 # for example: enable initcontent module
 
-#echo "drush fra"
-#drush -r $PARAM_PROJECT_PATH_APP fra -y
+drush -r $PARAM_PROJECT_PATH_APP dis coder devel -y
 
-#echo "drush en file_redirect"
-#drush -r $PARAM_PROJECT_PATH_APP en file_redirect -y
-#drush -r $PARAM_PROJECT_PATH_APP php-eval "variable_set('file_redirect_production_host', 'http://www.drupalcampwroclaw.pl');"
-#drush -r $PARAM_PROJECT_PATH_APP php-eval " file_redirect_create_htaccess();"
-
-
-#echo "drush en initcontent"
-#drush -r $PARAM_PROJECT_PATH_APP en initcontent -y
+drush -r $PARAM_PROJECT_PATH_APP en stage_file_proxy -y
+drush -r $PARAM_PROJECT_PATH_APP rr
+drush -r $PARAM_PROJECT_PATH_APP variable-set stage_file_proxy_origin "http://www.drupalcampwroclaw.pl"
 
 echo "Post-build script end"
 echo "==================="
