9cb26566823f14dd1431b8035bc27f73e1b733a1
bartlomiej.filipiuk@droptica.pl <bartlomiej.filipiuk@droptica.pl>
2015-03-16 15:05:59 +0100

    Issue #8355: validate palnned attendande field.

---
+++ app/sites/all/modules/custom/userprofile/js/script.js
@@ -0,0 +1,24 @@
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+    Drupal.behaviors.hide_profile_fields = {
+        attach: function(context, settings) {
+
+
+            $("input[name='field_attendance[und][no]']").live('click', function(){
+
+                $('.form-item-field-attendance-und-friday').toggle("slow");
+                $('.form-item-field-attendance-und-saturday').toggle("slow");
+                $('.form-item-field-attendance-und-sunday').toggle("slow");
+
+                $('input:checkbox[value=friday]').attr('checked',false);
+                $('input:checkbox[value=saturday]').attr('checked',false);
+                $('input:checkbox[value=sunday]').attr('checked',false);
+
+            });
+        }
+    };
+
+
+})(jQuery, Drupal, this, this.document);
